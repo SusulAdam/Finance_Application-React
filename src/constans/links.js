0c95209a5dd6f2b964cb065a6817f141ab55762a
Adam Susuł <a.w.susul@gmail.com>
2020-09-12 19:29:35 +0200

    Add new style on navigation implement new hover and active style and add logo

--- src/constans/links.js
+++ src/constans/links.js
@@ -4,9 +4,9 @@ import ManageFinances from 'layouts/Pages/ManageFinances'
 
 
 const links = [
-    { id: 1, name: "start", path: "/", exact: true, component: HomePage },
-    { id: 2, name: "manage-finances", path: "/manage-finances", component: ManageFinances },
-    { id: 3, name: "exchange-rate", path: "/exchange-rate", component: ExchangeRate },
+    { id: 1, name: "Start", path: "/", exact: true, component: HomePage },
+    { id: 2, name: "Manage-finances", path: "/manage-finances", component: ManageFinances },
+    { id: 3, name: "Exchange-rate", path: "/exchange-rate", component: ExchangeRate },
 
 ]
 
--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { links } from 'constans/links';
 import 'styles/main.scss';
+import { GiPig } from 'react-icons/gi';
 
 
 const Navigation = () => {
 
     const menu = links.map(link => (
-        <li key={link.name}>
+
+        <li className="navigation__li" key={link.name}>
             <NavLink to={link.path} exact={link.exact ? link.exact : false}> {link.name}</NavLink>
         </li>
+
     ))
 
     return (
         <ul>
+            <GiPig className="pigIcon"/>
+            <li><a className="pigBank-logo">PiggyBank</a></li>
             {menu}
+
         </ul>
     );
 
